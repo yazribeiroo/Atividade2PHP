74df23e73fdb9fab2eb74f786a475f6b5c049c39
yazribeiroo <yasmim.rm05@gmail.com>
2025-05-05 19:16:24 -0300

    Add files via upload

--- A2quest3.php
+++ A2quest3.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
+function calculargraos() {
+    $totalCasas = 64;
+    $graos = 0;
 
+    for ($i = 0; $i < $totalCasas; $i++) {
+        $graos += pow(2, $i);
+    }
 
+    return $graos;
+}
 
-
-
-
-
-
-
-
-
-
- ?>
+$resultado = calculargraos();
+echo "O número total de grãos que o monge receberia é: " . $resultado;
+?>
